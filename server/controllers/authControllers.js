e89eac7466331337f344ac93c8dad6ab8314aa84
bertcode21 <ekongbert02@gmail.com>
2025-12-16 22:32:00 +0100

    v4

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -2,6 +2,7 @@ const Users = require('./../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncErrorHandler = require('./../middleware/asyncErrorHandler');
+const { token } = require('morgan');
 
 exports.signup = asyncErrorHandler( async (req,res)=>{
     const password = req.body.password
@@ -41,11 +42,37 @@ req.body.password = hashedpassword;
 })
 exports.login = asyncErrorHandler(async (req, res) =>{
     try{
+        /// check if user exists
+  const user = await Users.findOne({ email: req.body.email})
+  if(!user){
+    res.send({
+        message: "OOps sorry the user does not exists",
+        success: false
+    })
+  }
 
+        // 2. check if user email and passowrd are correct
+      const isvalid = await bcrypt.compare(req.body.password, user.password)
+           if(!isvalid){
+         res.send({
+        message: "password incorrect",
+        success: false
+             })
+               }
+        /// 3. if user exists and password is correct anssign JWT.
+
+       const token =  jwt.sign({userId: user._id}, process.env.Secret_Key, {expiresIn: "1d"})
+    // console.log("user logged in")
+        res.send({
+            message: "User Logged in Succesfully",
+            success: true,
+            token: token
+        })
  }catch(error){
     res.send({
     message: "Error in Login",
     success: false,
+   
     
   })
  }
